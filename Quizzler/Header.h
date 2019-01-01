a8460cf4e56fc6e49514391568234f168151205e
Yairo Fernandez <fernandezyairo@gmail.com>
2019-01-01 16:36:30 -0500

    Added custom animation

---
+++ Quizzler/Header.h
@@ -0,0 +1,9 @@
+//
+//  Header.h
+//  Quizzler
+//
+//  Created by Yairo Fernandez on 1/1/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#import "ProgressHUD.h"
---
+++ Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Yairo Fernandez on 12/29/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
